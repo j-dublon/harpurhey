c4c9439e2bf6dd49db395222e83de8f321eed24b
j-dublon <j.johnson.dublon@gmail.com>
2020-07-16 17:00:18 +0100

    add basic structure to activities page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,39 +24,37 @@ const About = () => {
     <main className="about">
       <h1 className="about__title">Harpurhey Neighbourhood Project</h1>
       <section className="about__content">
-        <section className="about__content__top">
-          <img
-            src={require("../images/about.png")}
-            alt="A positive force banner"
-            className="about__content__top__image"
-          />
-          <p className="about__content__top__text">
-            HNP is a voluntary organisation which supports community activity in
-            Harpurhey, Manchester. The Centre is a venue for a variety of
-            activities, and a base for local groups.
-          </p>
-        </section>
-        <section className="about__content__history">
-          <h2 className="about__content__history__title">History</h2>
-          <p className="about__content__history__text">
-            The Centre was set up in .... and then this happened... and some
-            other stuff.
-          </p>
-        </section>
-        <section className="about__content__team">
-          <h2 className="about__content__team__title">Meet the team</h2>
-          {team.map((member) => {
-            return (
-              <TeamCard
-                bio={member.bio}
-                name={member.name}
-                role={member.role}
-                image={member.image}
-                key={member.key}
-              />
-            );
-          })}
-        </section>
+        <img
+          src={require("../images/about.png")}
+          alt="A positive force banner"
+          className="about__content__top__image"
+        />
+        <p className="about__content__top__text">
+          HNP is a voluntary organisation which supports community activity in
+          Harpurhey, Manchester. The Centre is a venue for a variety of
+          activities, and a base for local groups.
+        </p>
+      </section>
+      <section className="about__content__history">
+        <h2 className="about__content__history__title">History</h2>
+        <p className="about__content__history__text">
+          The Centre was set up in .... and then this happened... and some other
+          stuff.
+        </p>
+      </section>
+      <section className="about__content__team">
+        <h2 className="about__content__team__title">Meet the team</h2>
+        {team.map((member) => {
+          return (
+            <TeamCard
+              bio={member.bio}
+              name={member.name}
+              role={member.role}
+              image={member.image}
+              key={member.key}
+            />
+          );
+        })}
       </section>
     </main>
   );
--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Activities = () => {
-  return <p>Activities page</p>;
+  return (
+    <main className="activities">
+      <h1 className="activities__title">Activities</h1>
+      <section className="activities__content">
+        <section>
+          <h2 className="activities__content__title">Project 1</h2>
+          <p className="activities__content__text">
+            Information about project 1....
+          </p>
+        </section>
+        <img
+          src={require("../images/1.png")}
+          alt="Project 1"
+          className="activities__content__image"
+        />
+      </section>
+      <section className="activities__content">
+        <section>
+          <h2 className="activities__content__title">Project 2</h2>
+          <p className="activities__content__text">
+            Information about project 2....
+          </p>
+        </section>
+        <img
+          src={require("../images/2.png")}
+          alt="Project 2"
+          className="activities__content__image"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default Activities;
